e30272a562902430a61076f604e28dc14df0c46f
usamaforu <usamariaz113@gmail.com>
2023-01-24 21:05:59 +0500

    Update Class1.cs

--- Dtos/Class1.cs
+++ Dtos/Class1.cs
@@ -2,7 +2,7 @@
 {
     public class Class1
     {
-        public int MyProperty { get; set; }
+        public int id { get; set; }
         public int age { get; set; }
         public string Address { get; set; }
         public int mark { get; set; }
