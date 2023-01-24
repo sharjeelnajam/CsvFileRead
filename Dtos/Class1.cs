5701ba776e47db2d1d4b85f56d38e3f615c8c46b
sharjeelnajam <najamsharjeel@gmail.com>
2023-01-24 21:07:05 +0500

    class modify

--- Dtos/Class1.cs
+++ Dtos/Class1.cs
@@ -2,11 +2,12 @@
 {
     public class Class1
     {
-        public int MyProperty { get; set; }
+        public int StdId { get; set; }
         public int age { get; set; }
         public string Address { get; set; }
         public int mark { get; set; }
         public string cnic { get; set; }
         public string Position { get; set; }
+
     }
 }
